2c71543d5309d8fed87ee3ed92f0b63cb2a0b2ef
VaNomad <sjur.hassel@gmail.com>
2022-09-15 15:31:46 +0100

    first api call for gallery page

--- js/gallery.js
+++ js/gallery.js
@@ -1,2 +1,25 @@
+const url = "https://analogflowers.sjur.io/wp-json/wp/v2/pages/196"
+const urlb = "https://analogflowers.sjur.io/196-2/"
 const container = document.querySelector(".container");
+const gallery = document.querySelector(".gallery");
 
+async function createGallery() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+   
+    for (let i = 0; i <= data.length; i++) {
+      container.innerHTML += `
+      <img src="${data[i].image}"/>
+      <h1>"${data[i].image.name}"</h1>
+      `
+    }
+  } catch (error) {
+    console.log(error);
+    container.innerHTML = ("The image can not be found", error)
+    
+  }
+}
+
+createGallery();
