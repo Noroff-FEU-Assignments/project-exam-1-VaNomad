6016363b3c253ee4431d21edabadd36a952d9452
VaNomad <sjur.hassel@gmail.com>
2022-09-17 20:06:11 +0100

    added blog api call and html

---
+++ js/blog.js
@@ -0,0 +1,37 @@
+const url = "https://analogflowers.sjur.io/wp-json/wp/v2/posts?per_page=100&_embed"
+
+const container = document.querySelector(".blog-grid");
+
+async function createCarousel() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    
+    container.innerHTML = "";
+   
+    createHtml(data);
+
+  } catch (error) {
+    console.log(error);
+    container.innerHTML = ("The resource can not be found", error)
+    
+  }
+}
+
+createCarousel();
+
+
+function createHtml(data) {
+  for (let i = 0; i < data.length; i++) {
+    container.innerHTML += 
+                  `
+                  <a href="details.html?id=${data[i].id}" class="slide">
+                    <img src="${data[i]._embedded['wp:featuredmedia']['0'].source_url}">
+                    <div>  
+                      <h1 class="date">"${data[i].title.rendered}"</h1>
+                    </div>
+                  </a>
+                  `
+  }
+}
