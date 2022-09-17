e7d203b34a396846134f06b6137c49fc966e7c78
VaNomad <sjur.hassel@gmail.com>
2022-09-17 19:29:20 +0100

    query string url retrieval for details page

---
+++ js/details.js
@@ -0,0 +1,45 @@
+const postDetails = document.querySelector(".post-details");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+console.log(id);
+
+const url = "https://analogflowers.sjur.io/wp-json/wp/v2/posts/" + id;
+
+// ?per_page=100&_embed
+
+async function createCarousel() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    
+    postDetails.innerHTML = "";
+   
+    createHtml(data);
+
+  } catch (error) {
+    console.log(error);
+    postDetails.innerHTML = ("The resource can not be found", error)
+    
+  }
+}
+
+createCarousel();
+
+
+function createHtml(data) {
+  postDetails.innerHTML = 
+                  `
+                  <div class="flex">
+                    <h1>${data.date}</h1>
+                    <div class="details-image" style="background-image" url("${data.content.rendered}")></div>
+                  </div>
+                    
+                  
+                  `
+}
