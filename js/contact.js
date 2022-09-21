ca8e3175bd364ec4dc6ee39b11baae162dfae53b
VaNomad <sjur.hassel@gmail.com>
2022-09-21 14:46:50 +0100

    form css and start of validation and errors

---
+++ js/contact.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("#contact-form");
+
+form.onsubmit = function () {
+  event.preventDefault();
+  
+  console.log(event);
+
+  const name = document.querySelector("#name");
+
+  console.log(name.value);
+  
+  
+  
+}
