5fd6762c139a6e2acd1465d0e2edef82d97f15b8
VaNomad <sjur.hassel@gmail.com>
2022-09-23 22:17:31 +0100

    Trying to add gallery still

--- js/gallery.js
+++ js/gallery.js
@@ -13,7 +13,7 @@ const params = new URLSearchParams(queryString);
 const id = params.get("id");
 console.log(id);
 
-const gurl = "https://analogflowers.sjur.io/wp-json/wp/v2/media?per_page=100&_embed" + id;
+const gurl = "https://analogflowers.sjur.io/wp-json/wp/v2/posts?per_page=100&_embed" + id;
 
 // Gallery API call
 async function createGallery() {
@@ -27,8 +27,8 @@ async function createGallery() {
     for (let i = 0; i < data.length; i++) {
       gallery.innerHTML += `
                             <div class="card-container">
-                              <img src="${data[i].source_url}">
-                              <h1>"${data[i].caption.rendered}"</h1>
+                              <img src="${data[i].content.rendered}">
+                              <h1>"${data[i].title.rendered}"</h1>
                             </div>
       `
     }
