cb499f323baebb08bc07e1e2f4bc42038776bcd1
VaNomad <sjur.hassel@gmail.com>
2022-09-24 11:38:25 +0100

    gallery js cleaning

--- js/gallery.js
+++ js/gallery.js
@@ -1,23 +1,12 @@
 
 /* —————————  Gallery Page  ————————————————————————————————————————————— */
 
-// Gallery Endpoint
-
-
 // Gallery Targets
 const galleryContainer = document.querySelector(".gallery-container");
 const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
 
-console.log(galleryContainer);
-console.log(gallery);
-
-
-// const queryString = document.location.search;
-// const params = new URLSearchParams(queryString);
-// const id = params.get("id");
-// console.log(id);
-
+// Gallery Endpoint
 const gurl = "https://analogflowers.sjur.io/wp-json/wp/v2/pages/235?_embed";
 console.log(gurl);
 
@@ -34,22 +23,7 @@ async function createGallery() {
                             <div class="gallery-image">
                               ${data.content.rendered}
                              </div>
-    `
-   
-    // for (let i = 0; i < data.length; i++) {
-    //   gallery.innerHTML += `
-    //                         <img src="${data[i]}"/>
-    //                         <h1>"${data[i].title}"</h1>
-    //   `
-    // }
-
-    // data.forEach(function () {
-    //   gallery.innerHTML += `
-    //                         <div>
-    //                           <img src="${data.content.rendered}"/>
-    //                         </div>
-    //                       `
-    // });  
+    ` 
 
   } catch (error) {
     console.log(error);
