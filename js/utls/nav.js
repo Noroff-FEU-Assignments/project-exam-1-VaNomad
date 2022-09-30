b97e045629c526674f62e93f4961cf339cb0f28a
VaNomad <sjur.hassel@gmail.com>
2022-09-30 12:17:37 +0100

    z index title media query

--- js/utls/nav.js
+++ js/utls/nav.js
@@ -13,8 +13,6 @@ const navMenu = document.querySelector(".nav-menu");
 const mobileMenu = () => {
   mobile.classList.toggle("active");
   navMenu.classList.toggle("active");
-  console.log("mobile menu toggle is working");
-  
 }
 
 mobile.addEventListener("click", mobileMenu);
